eea4b35be879583e4b27887a2ee6f99ae955cb26
Freddie Aguilar <freddie.aguilar@streamlineverify.com>
2026-03-25 23:58:53 +0800

    Rename to index.html and remove API key for secure hosting

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'streamlineverify-glossary.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Initialize Database
